9efc3b6bd19a8229a73fe84a868603a52d179c5f
Me <me@example.com>
2026-09-13 07:56:52 +0800

    Fix kotlinx-serialization plugin id

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
 }
 
 
