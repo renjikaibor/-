b1747173a48d7550867fe3fdb11ebe6b05804af0
Me <me@example.com>
2026-09-13 07:38:39 +0800

    Fix KSP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.14" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
 }
 
 allprojects {
